190ffc32af46f9caa9dd3d5f05ac4c34407dd864
Boris Serebrov <seb.goo@gmail.com>
2013-07-22 20:41:23 +0300

    Add shell script to update vim from repository

---
+++ vim-update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd ~/vim
+# hg clone https://code.google.com/p/vim/
+hg pull
+hg update
+
+./configure --with-features=huge \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74a
+
+sudo  checkinstall
