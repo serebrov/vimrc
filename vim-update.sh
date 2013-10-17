f0926175d5266172d0b8dc8a87b21ac26a9e47fc
seb <serebrov@gmail.com>
2013-10-17 17:07:28 +0300

    Add update alternatives to the vim update script

--- vim-update.sh
+++ vim-update.sh
@@ -14,3 +14,8 @@ hg update
 make VIMRUNTIMEDIR=/usr/share/vim/vim74
 
 sudo  checkinstall -y
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
