9347eb520792730a93afc94e9230eecd9f6af72c
Boris Serebrov <serebrov@gmail.com>
2014-08-09 18:03:10 +0300

    Configure dpkg to not to update vim automatically

--- vim-update.sh
+++ vim-update.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-cd ~/vim
-# hg clone https://code.google.com/p/vim/
+# https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+
+if [[ -d ~/vim ]]; then
+  cd ~/vim
+else
+  sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial
+  sudo apt-get -y remove vim vim-runtime gvim
+  sudo apt-get -y install checkinstall
+  #mkdir ~/vim
+  #cd ~/vim
+  cd ~
+  hg clone https://code.google.com/p/vim/
+  cd ~/vim
+fi
 hg pull
 hg update
 
@@ -13,9 +27,14 @@ hg update
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim74
 
-sudo  checkinstall -y
+sudo checkinstall -y
 
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
 sudo update-alternatives --set editor /usr/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
 sudo update-alternatives --set vi /usr/bin/vim
+
+# prevent auto-update
+echo "vim hold" | sudo dpkg --set-selections
+echo "vim-common hold" | sudo dpkg --set-selections
+echo "vim-runtime hold" | sudo dpkg --set-selections
