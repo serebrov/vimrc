7e28031a3af70fbf553335c803bd139422755baa
Boris Serebrov <serebrov@gmail.com>
2019-10-17 22:03:40 +0300

    Fix paths in the init script

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ ! -f ~/vim/autoload/plug.vim ]]; then
-  pushd ~/vim
+if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
+  pushd ~/.vim
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   popd
 fi
 
+# for vim
+ln -s ~/.vim/.vimrc ~/.vimrc
+
 # for neovim
 ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vim/.vimrc ~/.vim/init.vim
+ln -s ~/.vim/.vimrc ~/.config/nvim/init.vim
