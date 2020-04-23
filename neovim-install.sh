9267f6f2ae9663cac1375e08eef35b4c98835d50
Boris Serebrov <serebrov@gmail.com>
2020-04-24 00:44:21 +0300

    Don't use pip2

--- neovim-install.sh
+++ neovim-install.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 sudo apt-get install neovim
 
 sudo apt-get install libffi-dev
-pip2 install --user neovim
-pip3 install --user neovim
+# pip2 install --user neovim
+pip install --user neovim
